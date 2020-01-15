4ef0aa86883698562ecc39e1cca6daa0f939a2d1
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-15 23:39:10 +0530

    Created basic codes in main.dart

--- coffee_finder_app/lib/main.dart
+++ coffee_finder_app/lib/main.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  return runApp(
+    new MyApp()
+  );
+}
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "My Coffess Shops",
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyMapPage(
+        title: "My Coffee Shops",
+      )
+    );
+  }
+
+}
