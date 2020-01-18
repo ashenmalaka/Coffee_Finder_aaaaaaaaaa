1973d6d70437651ec91d9989dfe4600aff4ce2a7
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-18 22:56:23 +0530

    Added CoffeeShopsData.dart with shop details class

---
+++ coffee_finder_app/lib/model/CoffeeShopsData.dart
@@ -0,0 +1,26 @@
+import 'package:google_maps_webservice/places.dart';
+
+class CoffeeShopsData {
+
+}
+
+class Shop{
+  final String id;
+  final String name;
+  final double lat;
+  final double lon;
+  final String photoRef;
+
+  Shop(this.id, this.name, this.lat, this.lon, this.photoRef);
+
+  static shopDetails(PlacesSearchResult places){
+    String id = places.id;
+    String name = places.name;
+    double lat = places.geometry.location.lat;
+    double lon = places.geometry.location.lng;
+    String photoRef = places.photos[0].photoReference;
+
+    return Shop(id, name, lat, lon, photoRef);
+
+  }
+}
