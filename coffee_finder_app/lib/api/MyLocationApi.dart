99a6b304007e5f58846782976e052aacd06d30dc
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-18 22:19:04 +0530

    Added error checking first

--- coffee_finder_app/lib/api/MyLocationApi.dart
+++ coffee_finder_app/lib/api/MyLocationApi.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_finder_app/model/LocationData.dart';
 import 'package:location/location.dart';
 
 class MyLocationApi{
@@ -7,5 +8,19 @@ class MyLocationApi{
 
   static MyLocationApi _instance;
   static MyLocationApi getInstance() => _instance ??= MyLocationApi();
-  
+
+  Future<MyLocationData> getLocation() async {
+    Map<String, double> location;
+
+    try{
+
+    }on PlatformException catch(e){
+      if(e.code == 'PERMISSION_DENIED'){
+        error = 'Permission denied';
+      }else if(e.code == 'PERMISSION_DENIED_NEVER_ASK'){
+        error = 'Permission denied - please ask user to enable it from settings';
+      }
+      location = null;
+    }
+  }
 }
