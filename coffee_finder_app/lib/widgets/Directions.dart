61105f4efbbe4d2ccef5d969b7b563c10df412c0
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-21 23:10:15 +0530

    Added directions.dart

---
+++ coffee_finder_app/lib/widgets/Directions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class Directions extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
+  
